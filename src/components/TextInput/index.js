a8883642bb91506ba31112e63f7417d06865b180
David Narbutovich <vamnemne@gmail.com>
2018-12-27 11:20:13 +0300

    Validate hack

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -1,8 +1,14 @@
 // @flow
 import { TextInput } from 'react-native';
 
+// $FlowFixMe
+if (!TextInput._onPress) {
+  throw new Error('This version React Native not support hack for TextInput!');
+}
+
 class TextInputCustom extends TextInput {
   _onPress = event => {
+    // $FlowFixMe
     const { onPress, editable } = this.props;
 
     if (onPress) {
