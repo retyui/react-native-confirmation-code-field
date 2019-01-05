9519d4ff19eb5d3ec856f7d379a2d80fe421d78f
David Narbutovich <vamnemne@gmail.com>
2019-01-05 16:11:47 +0300

    Fix test

--- src/components/ConfirmationCodeInput/ConfirmationCodeInput.test.js
+++ src/components/ConfirmationCodeInput/ConfirmationCodeInput.test.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import TextInputCustom from '../TextInputCustom';
-import Cell from '../TextCustom';
+import Cell from '../Cell';
 
 import ConfirmationCodeInput from './ConfirmationCodeInput';
 
