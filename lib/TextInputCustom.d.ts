29c9d4ebef61c2682fda47b163dbb25545303262
Mohamed Meabed <mo.meabed@gmail.com>
2020-03-27 02:16:37 +0400

    chore: update

--- lib/TextInputCustom.d.ts
+++ lib/TextInputCustom.d.ts
@@ -1,6 +1,7 @@
+import React from 'react';
 import { TextInputProps } from 'react-native';
 export interface ITextInputCustomProps extends TextInputProps {
     ref: any;
 }
-declare const TextInputCustom: (props: ITextInputCustomProps) => any;
+declare const TextInputCustom: (props: ITextInputCustomProps) => React.ReactElement<any, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)> | null) | (new (props: any) => React.Component<any, any, any>)>;
 export default TextInputCustom;
--- src/TextInputCustom.tsx
+++ src/TextInputCustom.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ReactElement} from 'react';
 import {
   NativeSyntheticEvent,
   TextInput,
@@ -6,13 +6,12 @@ import {
   TextInputProps,
 } from 'react-native';
 
+// eslint-disable-next-line @typescript-eslint/interface-name-prefix
 export interface ITextInputCustomProps extends TextInputProps {
   ref: any;
 }
 
-const TextInputCustom = (
-  props: ITextInputCustomProps,
-): typeof TextInput | any => {
+const TextInputCustom = (props: ITextInputCustomProps): ReactElement => {
   const {onKeyPress, editable} = props;
   // This hack needs to get click position and then calculate what cell on clicked
   const _onKeyPress = (
@@ -21,8 +20,8 @@ const TextInputCustom = (
     if (onKeyPress && (editable || editable === undefined)) {
       onKeyPress(event);
     }
-    return <TextInput onKeyPress={_onKeyPress} editable={editable} />;
   };
+  return <TextInput onKeyPress={_onKeyPress} editable={editable} />;
 };
 
 export default TextInputCustom;
